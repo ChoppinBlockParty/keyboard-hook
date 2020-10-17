93bb6f5a4b231c5e6f6f19bd58f6f4c717c1c5a0
Yuki <choppin.block.party@gmail.com>
2020-10-17 14:39:01 +0100

    Fix keyboard-hook-service.sh

--- keyboard-hook-service.sh
+++ keyboard-hook-service.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-device_ids=$(KeyboardHookReader | grep -i "keyboard" | cut -d' ' -f2 | perl -pe 's/.*([0-9]+)$/\1/')
+device_ids=$(KeyboardHookReader | grep -i "keyboard" | cut -d' ' -f2 | perl -pe 's/.*?([0-9]+)$/\1/')
 
 for i in $device_ids; do
     echo "/dev/input/event$i"
